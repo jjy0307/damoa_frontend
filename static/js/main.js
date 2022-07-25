daaab55f9f72b748df6214704fb070d7f6f431f0
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-07-26 00:18:20 +0900

    MOD main.js
    로그인시 화면 변하게 설정
    jwt payload정보 보내는건 하는중(보내는건 성공, 백에서 데이터 가공중)
    로그아웃은 공통적으로 사용될거로 보아 nav.barjs로 이동

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,39 @@
 async function if_login() {
-    console.log('byby');
-    let nav_bar = document.getElementsById("nav_bar");
-    nav_bar.setAttribute("style", "display:none")
+    let nav_bar_login = document.getElementById('nav_login_btn');
+    let nav_bar_logout = document.getElementById('nav_logout_btn');
+    nav_bar_login.setAttribute("style", "display:none");
+    nav_bar_logout.setAttribute("style", "display:flex");
+
+    let recommendation = document.getElementById('by_recommendation');
+    let joined = document.getElementById('by_joined');
+    recommendation.setAttribute("style", "display:none");
+    joined.setAttribute("style", "display:flex");
+
+    fetch("http://127.0.0.1:8000/community/main/", {
+        headers:{
+            Authorization : "Bearer " + localStorage.getItem("access"),
+        },
+      })
+        .then((response) => response.json())
+        // .then((result) => {
+        //     if (result.user_id) {
+        //         alert(result.user_id)
+        //     }
+        //     if (result.username) {
+        //         alert(result.username)
+        //     }
+        //     alert('회원 가입이 완료 되었습니다')
+        //     window.location.href = 'http://127.0.0.1:5500/login.html'
+        // })
+
 };
 
 window.onload = ()=>{
     const payload = JSON.parse(localStorage.getItem("payload"));
     // 아직 access 토큰의 인가 유효시간이 남은 경우
-    console.log('ㅗㅑㅗㅑㅗㅑㅗㅑ')
-    console.log(payload.exp)
-    console.log(Date.now()/1000)
     if (payload.exp > (Date.now() / 1000)){
-        console.log('hihi')
         if_login();
     } else {
-        console.log('설마')
         // 인증 시간이 지났기 때문에 다시 refreshToken으로 다시 요청을 해야 한다.
         const requestRefreshToken = async (url) => {
               const response = await fetch(url, {
@@ -34,18 +53,7 @@ window.onload = ()=>{
             // 새롭게 발급 받은 accessToken을 localStorage에 저장
             const accessToken = data.access;
             localStorage.setItem("access", accessToken);
-            console.log(accessToken)
-            console.log(access.exp)
+            if_login();
         });
     }
 };
-
-// 로그아웃 부분
-async function handleLogout(){
-    localStorage.removeItem("payload");
-    localStorage.removeItem("refresh");
-    localStorage.removeItem("access");
-    alert("로그아웃 되었습니다.")
-    location.reload()
-    window.location.replace(`login.html`);
-}
