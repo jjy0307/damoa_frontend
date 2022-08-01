64dbfdac563b1491544ca77c1018c933ff488e79
soiyo <soiyosauce@gmail.com>
2022-08-01 22:04:20 +0900

    MOD admin.js 새로고침해야 새로운정보 업데이트 되는 거 빼고 다 되었습니다. 보류시 보류했습니다, 삭제시 삭제하였습니다 alert 띄웁니다.

--- static/js/community/admin.js
+++ static/js/community/admin.js
@@ -3,7 +3,7 @@ function admin_articles() {
     fetch('http://127.0.0.1:8000/article/admin/')
         .then((response) => response.json())
         .then((json) => {
-            console.log(json);
+            // console.log(json);
             let hide_display = document.getElementById('article_and_comment_display');
             hide_display.setAttribute('style', 'display:none');
             let show_display = document.getElementById('right_side_item');
@@ -18,7 +18,7 @@ function admin_articles() {
             <th>제목</th>
             <th>작성자</th> 
             <th>작성일</th>
-            <th>조회</th>
+            <th>삭제여부</th>
         </tr>
         `;
             for (i = 0; i < json.length; i++) {
@@ -27,18 +27,17 @@ function admin_articles() {
                                                 <td id="click_${json[i]['id']}" onclick="article_id(this.id)">${json[i]['title']}</td>
                                                 <td>${json[i]['user_name']}</td>
                                                 <td>${json[i]['created_date'].slice(5, 10)}</td>
-                                                <td>조회수</td>`;
+                                                <button id="article_delete_${json[i]['id']}" onclick="article_yes_delete(this)">yes</button>
+                                                <button id="article_no_delete_${json[i]['id']}"onclick="article_no_delete(this)">no</button>`;
+
                 table.append(create_article_list);
             }
         });
 }
 function admin_comments() {
-    // let admin_article = document.getElementById('report_articles');
     fetch('http://127.0.0.1:8000/article/comment/admin/')
         .then((response) => response.json())
         .then((json) => {
-            console.log(json);
-
             let hide_display = document.getElementById('article_and_comment_display');
             hide_display.setAttribute('style', 'display:none');
             let show_display = document.getElementById('right_side_item');
@@ -53,7 +52,7 @@ function admin_comments() {
             <th>댓글내용</th>
             <th>작성자</th> 
             <th>작성일</th>
-            <th>조회</th>
+            <th>삭제여부</th>
         </tr>
         `;
             for (i = 0; i < json.length; i++) {
@@ -62,8 +61,77 @@ function admin_comments() {
                                                 <td id="click_${json[i]['id']}" onclick="article_id(this.id);">${json[i]['content']}</td>
                                                 <td>${json[i]['user_name']}</td>
                                                 <td>${json[i]['created_date'].slice(5, 10)}</td>
-                                                <td>조회수</td>`;
+                                                <button id="comment_delete_${json[i]['id']}" onclick="comment_yes_delete(this)">yes</button>
+                                                <button id="comment_no_delete_${json[i]['id']}"onclick="comment_no_delete(this)">no</button>`;
                 table.append(create_article_list);
             }
         });
+}
+//http://127.0.0.1:8000/article/1/write/delete/
+function article_yes_delete(delete_id) {
+    select_one = delete_id.id.split('_', 3)[2];
+    alert('게시글이 삭제되었습니다!');
+    fetch(`http://127.0.0.1:8000/article/${select_one}/write/delete/`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access'),
+        },
+    });
+}
+//http://127.0.0.1:8000/article/1/write/put/
+function article_no_delete(delete_id) {
+    select_one = delete_id.id.split('_', 4)[3];
+    alert('게시글이 보류되었습니다!');
+    fetch(`http://127.0.0.1:8000/article/${select_one}/write/`)
+        .then((response) => response.json())
+        .then((json) => {
+            return fetch(`http://127.0.0.1:8000/article/${select_one}/write/put/`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    title: json.title,
+                    content: json.content,
+                    is_valid: false,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    Authorization: 'Bearer ' + localStorage.getItem('access'),
+                },
+            });
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
+//http://127.0.0.1:8000/article/comment/7/write/delete/
+function comment_yes_delete(delete_id) {
+    select_one = delete_id.id.split('_', 3)[2];
+    alert('게시글이 삭제되었습니다!');
+    fetch(`http://127.0.0.1:8000/article/comment/${select_one}/write/delete/`, {
+        method: 'DELETE',
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access'),
+        },
+    });
+}
+//http://127.0.0.1:8000/article/comment/7/write/put/
+function comment_no_delete(delete_id) {
+    select_one = delete_id.id.split('_', 4)[3];
+    alert('게시글이 보류되었습니다!');
+    fetch(`http://127.0.0.1:8000/article/comment/${select_one}/write/`)
+        .then((response) => response.json())
+        .then((json) => {
+            return fetch(`http://127.0.0.1:8000/article/comment/${select_one}/write/put/`, {
+                method: 'PUT',
+                body: JSON.stringify({
+                    article: json.article,
+                    content: json.content,
+                    is_valid: false,
+                }),
+                headers: {
+                    'Content-type': 'application/json; charset=UTF-8',
+                    Authorization: 'Bearer ' + localStorage.getItem('access'),
+                },
+            });
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
 }
