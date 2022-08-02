bb73e9c7b3bd72b6c1521c668b1be6109ff48dce
soiyo <soiyosauce@gmail.com>
2022-08-02 21:15:24 +0900

    MOd write_article_post.js 수정

--- static/js/community/write_article_post.js
+++ static/js/community/write_article_post.js
@@ -1,33 +1,29 @@
 // 작성한 게시글 post하기
 async function postArticle() {
-    try {
-        let title = document.getElementById('article_title').value;
-        let content = document.getElementById('article_content').value;
-        let noticeboard = document.getElementById('noticeboard').value;
-        // let file = document.getElementById("article_file").value
-        let image = document.getElementById('article_image').files[0];
+    const title = document.getElementById('article_title').value;
+    const content = document.getElementById('article_content').value;
+    const noticeboard = document.getElementById('noticeboard').textContent;
+    const file = document.getElementById('article_file').files[0];
+    const image = document.getElementById('article_image').files;
 
-        let formdata = new FormData();
+    const formdata = new FormData();
 
-        formdata.append('title', title);
-        formdata.append('content', content);
-        // formdata.append('file', file)
-        formdata.append('image', image);
-        formdata.append('noticeboard', noticeboard);
+    formdata.append('title', title);
+    formdata.append('content', content);
+    formdata.append('file', file);
+    formdata.append('image', image);
+    formdata.append('noticeboard', noticeboard);
+    formdata.append('user_id', localStorage.getItem('payload'));
 
-        let response = await fetch('http://127.0.0.1:8000/article/write/', {
-            method: 'POST',
-            body: formdata,
-        });
+    const response = await fetch('http://127.0.0.1:8000/article/write/', {
+        method: 'POST',
+        body: formdata,
+    });
 
-        if (response.status == 200) {
-            alert('글작성 완료!');
-            window.location.replace('http://127.0.0.1:5500/');
-        } else {
-            alert(response.status);
-        }
-    } catch (error) {
-        console.error(error);
-        alert('작성한 게시글 post하기에서 오류가 발생했습니다!');
+    if (response.status == 200) {
+        alert('글작성 완료!');
+        // window.location.replace('http://127.0.0.1:5500/');
+    } else {
+        alert(response.status);
     }
 }
