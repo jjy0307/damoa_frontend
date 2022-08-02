28119a0d50bf89337330055e123d06398bdd65d4
soiyo <soiyosauce@gmail.com>
2022-08-02 21:15:40 +0900

    MOD write_article.js noticeboard id 부여

--- static/js/community/write_article.js
+++ static/js/community/write_article.js
@@ -1,9 +1,11 @@
 // 게시글 작성하는 부분 띄우기
 function write_article(clicked_id) {
     try {
-        let write_article_num = clicked_id.split('_', 5)[3];
-        let write_article_name = clicked_id.split('_', 5)[4];
-        console.log(write_article_num, write_article_name);
+        console.log(clicked_id);
+        let write_article_noticeboard_num = clicked_id.split('_', 5)[3];
+        let write_article_noticeborad_name = clicked_id.split('_', 5)[4];
+
+        console.log(write_article_noticeboard_num, write_article_noticeborad_name);
         let hide_display = document.getElementById('article_and_comment_display');
         hide_display.setAttribute('style', 'display:none');
         let hide_display_article = document.getElementById('right_side_item');
@@ -15,8 +17,8 @@ function write_article(clicked_id) {
 
         let article_mod_area = document.getElementById('article_mod_area');
         article_mod_area.innerHTML = `<div class="article_header">
-    <h2 id="noticeboard">${write_article_name}</h2>
-    <button type="button" onclick="postArticle()" class="article_write_button">작성</button>
+    <h2 id="noticeboard">${write_article_noticeborad_name}</h2>
+    <button id="wrtie_article_id_${write_article_noticeboard_num}" type="button" onclick="postArticle(this)" class="article_write_button">작성</button>
     </div>
     <hr />    
     <div class="article_writearea">
@@ -30,7 +32,7 @@ function write_article(clicked_id) {
     </div>
     <div >
     <input type="file" name="file" id="article_file"></input>
-    <input type="file" name="image" id="article_image"></input>
+    <input type="file" name="image" id="article_image" multiple></input>
     </div>
 `;
     } catch (error) {
