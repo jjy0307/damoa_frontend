680b0fa2f7d21d2157a7635ef04219d3c801b1fb
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-10 13:27:04 +0900

    MOD static/js/community/write_article.js
    Quill추가

--- static/js/community/write_article.js
+++ static/js/community/write_article.js
@@ -16,25 +16,43 @@ function write_article(clicked_id) {
         hide_display_all_article.setAttribute('style', 'display:none');
 
         let article_mod_area = document.getElementById('article_mod_area');
-        article_mod_area.innerHTML = `<div class="article_header">
-    <h2 id="noticeboard">${write_article_noticeborad_name}</h2>
-    <button id="wrtie_article_id_${write_article_noticeboard_num}" type="button" onclick="postArticle(this)" class="article_write_button">작성</button>
-    </div>
-    <hr />    
-    <div class="article_writearea">
-    <input class="article_title" type="text" name="title" id="article_title" placeholder="제목을 입력해 주세요."></input>
-    </div>
-    <hr />
-    <div class="text">
-    <div>
-        <textarea rows="38" cols="178" class="article_content" type="text" name="content" id="article_content" placeholder="내용을 입력해 주세요."></textarea>
-    </div>
-    </div>
-    <div >
-    <input type="file" name="file" id="article_file"></input>
-    <input type="file" name="image" id="article_image" multiple></input>
-    </div>
-`;
+        article_mod_area.innerHTML = `
+        <div class="article_header">
+            <h2 id="noticeboard">${write_article_noticeborad_name}</h2>
+            <button id="wrtie_article_id_${write_article_noticeboard_num}" type="button" onclick="postArticle(this)" class="article_write_button">작성</button>
+        </div>
+        <hr />    
+        <div class="article_mod_writearea">
+            <input class="article_title" type="text" name="title" id="article_title" placeholder="제목을 입력해 주세요."></input>
+        </div>
+        <hr />
+        <div class="article_mod_content">
+            <div id = "article_content"></div>
+        </div>
+        <div class="article_mod_file_area">
+            <input type="file" name="file" id="article_file"></input>
+            <input type="file" name="image" id="article_image" multiple></input>
+        </div>`;
+        let toolbarOptions = [
+            // [{font: ['Sans Serif', 'Serif', 'Monospace']}]
+            [{header : [1, 2, false]}],
+            [{ size: [ 'small', false, 'large', 'huge' ]}],
+            ['bold', 'italic', 'underline', 'strike'],
+            ['color', 'background'],
+            ['script'],
+            ['blockquote', 'code-block'],
+            ['list', 'indent', 'align', 'direction'],
+            ['link', 'image', 'video', 'formula']
+        ];
+        let quillEditor = new Quill(
+            '#article_content',
+            {
+                modules: {
+                    toolbar: toolbarOptions
+                },
+                theme:'snow'
+            }
+        );
     } catch (error) {
         console.error(error);
         alert('게시글 작성하는 부분 띄우기에서 오류가 발생했습니다!');
