541b13879b5adb212bd9f2e99b02a79fa711c63f
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-10 17:44:15 +0900

    MOD static/js/community/edit_article.js
    post put 한 파일로 수정

--- static/js/community/edit_article.js
+++ static/js/community/edit_article.js
@@ -1,11 +1,11 @@
 // 글 수정 페이지로 이동 및 불러오기
-function edit_article(clicked_id) {
+async function edit_article(clicked_id) {
     try {
-        console.log(clicked_id);
         article_num = clicked_id.split('_', 4)[3];
         fetch(`http://127.0.0.1:8000/article/${article_num}/write`)
             .then((response) => response.json())
             .then((json) => {
+                console.log(json)
                 let hide_display = document.getElementById('article_and_comment_display');
                 hide_display.setAttribute('style', 'display:none');
                 let show_display = document.getElementById('article_mod_wrap_area');
@@ -17,7 +17,7 @@ function edit_article(clicked_id) {
                 article_mod_area.innerHTML = `
                 <div class="article_header">
                     <h2>${json.noticeboard_name}</h2>
-                    <button id="article_mod_fin_${json.id}" class="article_write_button" onclick="edit_article_post(this.id)">수정 완료</button>
+                    <button id="article_mod_fin_${json.id}_${json.noticeboard_id}" class="article_write_button" onclick="edit_article_post(this.id)">수정 완료</button>
                 </div>
                 <hr />
                 <div class="article_mod_writearea">
@@ -70,5 +70,50 @@ function edit_article(clicked_id) {
     } catch (error) {
         console.error(error);
         alert('글 수정 페이지를 불러오는 중 에러가 발생했습니다!');
+    }
+}
+
+
+async function edit_article_post(clicked_id) {
+    try {
+        const article_num = clicked_id.split("article_mod_fin_")[1].split("_")[0];
+        const noticeboard_num = clicked_id.split("article_mod_fin_")[1].split("_")[1];
+        const title = document.getElementById('edit_article_title').value;
+        const content = document.getElementById('edit_article_content').getElementsByClassName('ql-editor')[0].innerHTML;
+        const file = document.getElementById('edit_article_file').files[0];
+        const formdata = new FormData();
+        console.log(article_num)
+        console.log(title)
+        console.log(content)
+        console.log(file)
+        console.log(noticeboard_num)
+        formdata.append('article', article_num);
+        formdata.append('noticeboard', noticeboard_num)
+        formdata.append('title', title);
+        formdata.append('content', content);
+        formdata.append('file', file);
+        formdata.append('user_id', localStorage.getItem('payload'));
+        await fetch('http://127.0.0.1:8000/article/write/', {
+            method: 'PUT',
+            body: formdata,
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            if (result.message==='contents_error'){
+                alert('제목과 내용은 필수 항목입니다.')
+            } 
+            if (result.message==='upload_error'){
+                alert('업로드 중 문제가 생겼습니다. 추후에 재시도 해보세요.')
+            }
+            if (result.message==='success'){
+                alert('작성 완료!');
+                window.location.reload();
+            }
+        });
+    
+    
+    } catch (error) {
+        console.error(error);
+        alert('게시글을 post하는 중 오류 발생!');
     }
 }
