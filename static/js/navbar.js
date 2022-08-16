6bf59cde3afaf6f8b12f25f2bb78621aa5599095
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-16 13:35:51 +0900

    MOD static/js/navbar.js
    경로 변경

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -5,20 +5,31 @@ async function handleLogout() {
     localStorage.removeItem('access');
     alert('로그아웃 되었습니다.');
     location.reload();
-    window.location.replace(`http://43.200.24.208/main`);
+
+    // [IP 변경]
+    // window.location.replace(`http://43.200.24.208/main`);
+    window.location.replace(`http://127.0.0.1:5500/main.html`);
 }
 
 async function handleLogin() {
-    window.location.replace(`http://43.200.24.208/login`);
+    // [IP 변경]
+    // window.location.replace(`http://43.200.24.208/login`);
+    window.location.replace(`http://127.0.0.1:5500/login.html`);
 }
 
 async function handleSignUp() {
-    window.location.replace(`http://43.200.24.208/signup`);
+    // [IP 변경]
+    // window.location.replace(`http://43.200.24.208/signup`);
+    window.location.replace(`http://127.0.0.1:5500/signup.html`);
 }
 
 async function handleMain() {
-    window.location.replace(`http://43.200.24.208/main`);
+    // [IP 변경]
+    // window.location.replace(`http://43.200.24.208/main`);
+    window.location.replace(`http://127.0.0.1:5500/main.html`);
 }
 async function handleMypage() {
-    window.location.replace(`http://43.200.24.208/mypage`);
+    // [IP 변경]
+    // window.location.replace(`http://43.200.24.208/mypage`);
+    window.location.replace(`http://127.0.0.1:5500/my_page.html`);
 }
