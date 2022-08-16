e6217577661ce9b62d09f9dd82b14a325878b8fb
Seungtae Lee <90381057+SingToLive@users.noreply.github.com>
2022-08-16 13:33:30 +0900

    MOD static/js/community/write_article.js
    경로 변경

--- static/js/community/write_article.js
+++ static/js/community/write_article.js
@@ -1,11 +1,9 @@
 // 게시글 작성하는 부분 띄우기
 function write_article(clicked_id) {
     try {
-        console.log(clicked_id);
         let write_article_noticeboard_num = clicked_id.split('_', 5)[3];
         let write_article_noticeborad_name = clicked_id.split('_', 5)[4];
 
-        console.log(write_article_noticeboard_num, write_article_noticeborad_name);
         let hide_display = document.getElementById('article_and_comment_display');
         hide_display.setAttribute('style', 'display:none');
         let hide_display_article = document.getElementById('right_side_item');
@@ -18,7 +16,7 @@ function write_article(clicked_id) {
         let article_mod_area = document.getElementById('article_mod_area');
         article_mod_area.innerHTML = `
         <div class="article_header">
-            <h2 id="noticeboard">${write_article_noticeborad_name}</h2>
+            <h2 id="noticeboard">${write_article_noticeborad_name} 게시판</h2>
             <button id="wrtie_article_id_${write_article_noticeboard_num}" type="button" onclick="postArticle(this)" class="article_write_button">작성</button>
         </div>
         <hr />    
@@ -31,7 +29,6 @@ function write_article(clicked_id) {
         </div>
         <div class="article_mod_file_area">
             <input type="file" name="file" id="article_file"></input>
-            <input type="file" name="image" id="article_image" multiple></input>
         </div>`;
         let toolbarOptions = [
             // [{font: ['Sans Serif', 'Serif', 'Monospace']}]
